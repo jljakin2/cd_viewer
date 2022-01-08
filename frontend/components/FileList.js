a4ccf23037df668456be9d39e3fbdb15b21bb049
jljakin2 <jeff.jakinovich@gmail.com>
2022-01-07 22:01:26 -0500

    list view of files in viewer

---
+++ frontend/components/FileList.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+import { usePreview } from "../lib/preview";
+
+const FileListStyles = styled.div`
+  cursor: pointer;
+  border-radius: 0.3125rem;
+  background: ${({ isActive }) =>
+    isActive ? "var(--greyWhite)" : "transparent"};
+  color: ${({ isActive }) => (isActive ? "var(--grey1)" : "var(--greyWhite)")};
+
+  display: grid;
+  grid:
+    "name date size" 1fr
+    / 2fr 1fr 1fr;
+
+  padding: 1rem;
+
+  &:hover {
+    background: ${({ isActive }) =>
+      isActive ? "var(--greyWhite)" : "var(--grey2)"};
+  }
+`;
+
+export default function FileList({ id, name, dateModified, size }) {
+  const router = useRouter();
+
+  const { handleFileClick, currentSelected } = usePreview();
+
+  const [isActive, setIsActive] = useState(currentSelected === id);
+
+  useEffect(() => {
+    setIsActive(currentSelected === id);
+  }, [currentSelected]);
+
+  return (
+    <FileListStyles
+      onDoubleClick={() => router.push(`/viewer/file/${id}`)}
+      onClick={handleFileClick}
+      isActive={isActive}
+      data-name={id}>
+      <p className="body-light">{name}</p>
+      <p className="body-light">{dateModified}</p>
+      <p className="body-light">{size}</p>
+    </FileListStyles>
+  );
+}
--- frontend/components/FileViewer.js
+++ frontend/components/FileViewer.js
@@ -1,31 +1,107 @@
+import { useState } from "react";
 import styled from "styled-components";
 import FileCard from "./FileCard";
+import CardViewIcon from "./Icons/CardViewIcon";
+import ListViewIcon from "./Icons/ListViewIcon";
+import FileList from "./FileList";
+import { usePreview } from "../lib/preview";
 
 const FileViewerStyles = styled.div`
   padding-left: 2.5rem;
 
-  h2 {
+  .file-container--card {
+    display: flex;
+    gap: 2rem;
+  }
+
+  .file-container--list {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    max-width: ${({ showPreview }) => (showPreview ? "100%" : "75%")};
+    padding-right: 1rem;
+
+    & .header {
+      display: grid;
+      grid:
+        "name date size" 1fr
+        / 2fr 1fr 1fr;
+
+      & #name {
+        grid-area: name;
+      }
+
+      & #modified {
+        grid-area: date;
+      }
+
+      & #size {
+        grid-area: size;
+      }
+    }
+  }
+
+  .top {
+    display: flex;
+    align-items: center;
+    column-gap: 3rem;
+
     margin-bottom: 2.5rem;
   }
 
-  .file-container {
+  .icons {
     display: flex;
-    gap: 2rem;
+    justify-content: center;
+    align-items: center;
+    column-gap: 1.5rem;
   }
 `;
 
 export default function FileViewer({ folderName, files }) {
-  const renderedFiles = files?.map(file => {
-    return <FileCard name={file.name} id={file.id} />;
+  const { showPreview } = usePreview();
+
+  const [isListView, setIsListView] = useState(false);
+
+  const renderedFileCards = files?.map(file => {
+    return <FileCard key={file.id} name={file.name} id={file.id} />;
+  });
+
+  const renderedFileLists = files?.map(file => {
+    return <FileList key={file.id} {...file} />;
   });
 
   return (
-    <FileViewerStyles>
-      <h2>
-        {folderName}{" "}
-        {`(${files?.length} ${files?.length === 1 ? "item" : "items"})`}
-      </h2>
-      <div className="file-container">{renderedFiles}</div>
+    <FileViewerStyles isListView={isListView} showPreview={showPreview}>
+      <div className="top">
+        <h2>
+          {folderName}{" "}
+          <span className="body-light">{`(${files?.length} ${
+            files?.length === 1 ? "item" : "items"
+          })`}</span>
+        </h2>
+        <div className="icons">
+          <div onClick={() => setIsListView(false)}>
+            <CardViewIcon active={!isListView} />
+          </div>
+          <div onClick={() => setIsListView(true)}>
+            <ListViewIcon active={isListView} />
+          </div>
+        </div>
+      </div>
+
+      {!isListView ? (
+        <div className="file-container--card">{renderedFileCards}</div>
+      ) : (
+        <div className="file-container--list">
+          <div className="header">
+            <h5 id="name">Name</h5>
+            <h5 id="modified">Date Modified</h5>
+            <h5 id="size">Size</h5>
+          </div>
+          {renderedFileLists}
+        </div>
+      )}
     </FileViewerStyles>
   );
 }
